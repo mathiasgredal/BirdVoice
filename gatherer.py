07f258349547f56d74ebda78bf0e42267967a89c
Mathias Gredal <Mathiasgredal@icloud.com>
2017-06-28 21:23:11 +0200

    Added more persistency in the sorting function

--- gatherer.py
+++ gatherer.py
@@ -1,9 +1,9 @@
 # Dependencies
 from collections import namedtuple
-import urllib2
-import json
+from urllib.request import urlopen
+import ujson as json
 import os.path
-
+from progress.bar import Bar
 # Modules
 
 #classes
@@ -11,8 +11,7 @@ class Species:
     speciesName= ""
     speciesRecordings = []
 
-    def speciesCount(self):
-        return len(self.speciesRecordings)
+    speciesCount = len(speciesRecordings)
 
     def __init__(self,species,recordings):
         self.speciesName = species
@@ -22,9 +21,14 @@ class Species:
         definition = (self.speciesName,self.recordings)
         return str(definition)
 
+    def __repr__(self):
+        return repr((self.speciesName, self.speciesCount, self.speciesRecordings))
+
 # Functions
 def DownloadApiJson():
     numPages,numSpecies = GetApiInfo()
+    print("Number of pages is: "+str(numPages))
+    print("Number of species is: "+str(numSpecies))
     recordings = []
 
     # check if apiJSON exists
@@ -34,8 +38,11 @@ def DownloadApiJson():
             recordings = json.load(infile)
         return recordings
     else:
-        for(i in numPages):
+        bar = Bar('IncrementalBar', max=numPages)
+        for i in range(1,numPages+1):
             recordings.append(DownloadOnePage(i))
+            bar.next()
+        bar.finish()
 
         # write this to file
         with open(apiJSONPath,"w") as outfile:
@@ -45,39 +52,55 @@ def DownloadApiJson():
 
 
 def DownloadOnePage(pageNum):
-    response = urllib2.urlopen(url+pageNum)
-    data = json.load(response).recordings
+    response = urlopen(url+str(pageNum))
+    data = json.load(response)["recordings"]
     return data
 
 def GetApiInfo():
-
-    response = urllib2.urlopen(url+"1")
+    response = urlopen(url+"1")
     data = json.load(response)
-    numSpecies = data.numSpecies
-    numPages = data.numPages
+    numSpecies = data["numSpecies"]
+    numPages = data["numPages"]
     return (numPages,numSpecies)
 
 
 def SortRecordings(recordings):
-    sortedSpecies = []
+    unsortedSpecies = []
     # here we set all the species
-    for (recording in recordings):
-        if(!SpeciesContains(recording.en,sortedSpecies)):
-            species = Species(recording.en,[recording])
-            sortedSpecies.append(species)
+    bar = Bar('IncrementalBar', max=len(recordings))
+    for page in recordings:
+        for recording in page:
+            if( SpeciesContains(recording["en"],unsortedSpecies) != True):
+                species = Species(recording["en"],[])
+                unsortedSpecies.append(species)
+        bar.next()
+    bar.finish()
     # now every single species is inside our sorted species
     # we can now fill up our species recordings
+    bar1 = Bar('IncrementalBar', max=len(recordings))
+    for page in recordings:
+        for recording in page:
+            for species in unsortedSpecies:
+                if(species.speciesName == recording["en"]):
+                    species.speciesRecordings.append(recording)
+                    break
+        bar1.next()
+    bar1.finish()
 
-    for(recording in recordings):
-        for (species in sortedSpecies):
-            if(species.speciesName == recording.en):
-                species.speciesRecordings.append(recording)
-                break
+    with open(sortedAPIJSONPath,"w") as outfile:
+        json.dump(unsortedSpecies,outfile)
+
+    # We can now begin the real sorting
+    sortedSpecies = sorted(unsortedSpecies, key=lambda species: species.speciesCount)
+    with open(sortedAPIJSONPath,"w") as outfile:
+        json.dump(sortedSpecies,outfile)
+
+
 
 
 def SpeciesContains(speciesName,sortedSpecies):
-    for (species in sortedSpecies):
-        if(species.speciesName == speciesName)
+    for species in sortedSpecies:
+        if(species.speciesName == speciesName):
             return True
     return False
 
@@ -86,5 +109,12 @@ def SpeciesContains(speciesName,sortedSpecies):
 url = "http://www.xeno-canto.org/api/2/recordings?query=nr:1-1000000&page="
 
 apiJSONPath = "recordings.json"
+sortedAPIJSONPath = "sortedRecordings.json"
+
 
 def StartGathering():
+    recordings = DownloadApiJson()
+    sortedRecordings = SortRecordings(recordings)
+    print(sortedRecordings)
+
+StartGathering()
